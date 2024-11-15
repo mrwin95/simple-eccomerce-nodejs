429eba6aff485a6981561c86cb3974a48cee03f3
Thang Nguyen <mrwin95@gmail.com>
2024-11-14 21:41:39 -0700

    improve overload function

--- src/helpers/check.connect.ts
+++ src/helpers/check.connect.ts
@@ -4,7 +4,7 @@ import { log } from "console";
 import mongoose from "mongoose";
 import os from "os";
 
-const _SECONDS = 10000;
+const _INTERVAL_MS = 10000;
 export const countConnect = () => {
   const numberConnection = mongoose.connections.length;
   log(`Number of connections: ${numberConnection}`);
@@ -15,16 +15,16 @@ export const checkOverLoad = () => {
   setInterval(() => {
     const numberConnection = mongoose.connections.length;
     const numberOfCores = os.cpus().length;
-    const memoryUsage = os.totalmem() - os.freemem();
+    const memoryUsageMB = (os.totalmem() - os.freemem()) / (1024 * 1024 * 1000);
     const maxConnection = numberOfCores * 5;
 
     log(`Number of connections: ${numberConnection}`);
     log(`Number of cores: ${numberOfCores}`);
-    log(`Memory usage: ${memoryUsage / 1024 / 1024 / 1000} MB`);
+    log(`Memory usage: ${memoryUsageMB.toFixed(2)} MB`);
 
     if (numberConnection > maxConnection) {
       log("Overload connections");
-      // send notification
+      // TODO: Add notification logic here
     }
-  }, _SECONDS);
+  }, _INTERVAL_MS);
 };
