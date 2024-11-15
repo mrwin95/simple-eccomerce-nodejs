9b70bc91136bff3a9c9f3aa92c5f36725302d096
Thang Nguyen <mrwin95@gmail.com>
2024-11-14 21:07:36 -0700

    added essentials middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+
 const app = express();
 
 // init middleware
-
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 // Routes
 
+app.get("/", (req, res) => {
+  const strCompress = "Hello World";
+
+  res
+    .status(200)
+    .json({ message: "Welcome", metadata: strCompress.repeat(1000) });
+});
 // Error handling
 
 export default app;
